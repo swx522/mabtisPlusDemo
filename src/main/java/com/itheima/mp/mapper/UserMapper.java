775e8e5af969acef981dbd95582108621bf4a100
swx522 <754607558@qq.com>
2026-04-13 11:15:29 +0800

    first

---
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.core.Constants;
+
+import java.util.List;
+
+public interface UserMapper extends BaseMapper<User> {
+    List<User> queryUserByIds(@Param("ids") List<Long> ids);
+
+    void updateBalanceByIds(@Param("ew")QueryWrapper<User> wrapper, int amount);
+
+}
